48be01bf1ea27e40017012af882d4a3e3afb501f
N0bl3 <alexandre.noble.pro@gmail.com>
2016-07-04 19:21:57 +0200

    Created main routes Returning 501

--- routes/all.js
+++ routes/all.js
@@ -1,3 +1,44 @@
 exports.index = (req, res) => {
-  res.send('Hello!!');
+  res.render('index');
+};
+//  Authenticate
+
+exports.authenticate = (req, res) => {
+  res.sendStatus(501);
+};
+
+//  Create
+exports.createPoll = (req, res) => {
+  res.sendStatus(501);
+};
+
+exports.createPollOption = (req, res) => {
+  res.sendStatus(501);
+};
+
+//  Read
+exports.getPoll = (req, res) => {
+  res.sendStatus(501);
+};
+
+exports.listUserPolls = (req, res) => {
+  res.sendStatus(501);
+};
+
+//  Update
+exports.sharePoll = (req, res) => {
+  res.sendStatus(501);
+};
+
+exports.votePoll = (req, res) => {
+  res.sendStatus(501);
+};
+
+//  Delete
+exports.deletePoll = (req, res) => {
+  res.sendStatus(501);
+};
+
+exports.deletePollOption = (req, res) => {
+  res.sendStatus(501);
 };
